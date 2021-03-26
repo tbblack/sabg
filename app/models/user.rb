d3badc0e9ad865d05c58e408b9ba57a6f76bb0db
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2021-03-25 23:44:01 -0300

    fix: instala e aplica a gem annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  nome            :string
+#  email           :string
+#  data_nascimento :string
+#  telefone        :string
+#  cpf             :string
+#  perfil          :integer
+#  situacao        :integer
+#  endereco        :string
+#  complemento     :string
+#  observacoes     :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
 end
