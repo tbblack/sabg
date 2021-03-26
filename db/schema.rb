07949fbb543c597ba2c494002b837ba69f496519
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2021-03-26 00:30:52 -0300

    fix: migrate aplicacao gem bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_023629) do
+ActiveRecord::Schema.define(version: 2021_03_26_032916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_03_26_023629) do
     t.string "observacoes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
