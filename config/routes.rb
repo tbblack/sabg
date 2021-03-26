a90f7570f302ebac0cd8387add21ed16deb3f035
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2021-03-26 01:47:59 -0300

    fix: corrigindo rota

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   devise_for :profiles
   devise_for :admins
   resources :users, only: [:create, :show]
-  get "/signup", to: "users#new"
-  get "/login", to: "sessions#new"
-  post "/sessions", to: "sessions#create"
-  delete "/sessions", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
